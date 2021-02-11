080d1564160c151a042711c72d66305dabda7a1b
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-11 07:46:59 -0300

    test: ensure BaseService.find returns type of related model

--- src/presentation/services/base-service.spec.ts
+++ src/presentation/services/base-service.spec.ts
@@ -3,10 +3,21 @@ import { Service } from '../protocols'
 import { Model } from '../protocols/model'
 import { BaseService } from './base-service'
 
-const makeSutWithNoResource = (): Service => {
-  interface User extends Model {
-    name: string
+interface User extends Model {
+  name: string
+}
+
+const makeSut = (): Service<User> => {
+  class UserService extends BaseService<User> {
+    get resource (): string {
+      return 'users'
+    }
   }
+
+  return new UserService()
+}
+
+const makeSutWithNoResource = (): Service<User> => {
   class UserService extends BaseService<User> {}
   return new UserService()
 }
@@ -16,5 +27,12 @@ describe('Base Service Class', () => {
     expect(() => makeSutWithNoResource()).toThrowError(
       new MissingResourceError()
     )
+  })
+
+  test('should BaseService returns Model with related id in find method', () => {
+    const sut = makeSut()
+    const result = sut.find(1)
+    expect(result).toBeTruthy()
+    expect(result.id).toBe(1)
   })
 })
