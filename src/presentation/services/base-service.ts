ef710fff28519be7c76a787b938b4505d9c9f849
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-13 13:15:51 -0300

    feat: add url builder to base service

--- src/presentation/services/base-service.ts
+++ src/presentation/services/base-service.ts
@@ -3,6 +3,8 @@ import { Service } from '../protocols'
 import { Form as BaseForm, Model as BaseModel } from '../../domain/models'
 import { HttpClient } from '../../domain/protocols'
 import { AxiosAdapter } from '../../infra/http-client/axios-adapter'
+import { URLBuilder } from '../protocols/url-builder'
+import { URLBuilder as URLBuilderImpl } from './url-builder'
 
 export abstract class BaseService<
   M extends BaseModel,
@@ -10,6 +12,7 @@ export abstract class BaseService<
   F extends BaseForm = Partial<M>
 > implements Service<M> {
   private readonly client: HttpClient
+  private readonly urlBuilder: URLBuilder
 
   get resource (): string {
     return ''
@@ -18,10 +21,15 @@ export abstract class BaseService<
   constructor () {
     if (!this.resource) throw new MissingResourceError()
     this.client = new AxiosAdapter()
+    this.urlBuilder = new URLBuilderImpl(this.resource)
   }
 
   async find (id: string | number): Promise<M> {
-    const { data } = await this.client.get(`${this.resource}/${id}`)
+    const { data } = await this.client.get(`${this.uri}/${id}`)
     return data
+  }
+
+  private get uri (): string {
+    return this.urlBuilder.handler()
   }
 }
