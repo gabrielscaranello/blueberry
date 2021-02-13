4a1e1bc86281e42876eb6255d7fe194c3d15062b
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-13 14:19:11 -0300

    refactor: add missign types to Service interface

--- src/presentation/protocols/service.ts
+++ src/presentation/protocols/service.ts
@@ -3,4 +3,9 @@ import { Model } from '../../domain/models'
 export interface Service<M extends Model> {
   readonly resource: string
   find: (id: number | string) => Promise<M>
+  params: (param: number | string) => Service<M>
+  search: (search: number | string) => Service<M>
+  page: (page: number) => Service<M>
+  limit: (limit: number) => Service<M>
+  query: (field: string, value: number | string) => Service<M>
 }
