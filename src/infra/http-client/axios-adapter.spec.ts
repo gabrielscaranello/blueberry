34ff310401e24a048de71cb103543f7a0b4a3268
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-13 12:14:20 -0300

    test: ensure AxiosAdapter returns error if no baseURL is provided

--- src/infra/http-client/axios-adapter.spec.ts
+++ src/infra/http-client/axios-adapter.spec.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { AxiosAdapter } from './axios-adapter'
 import { HttpResponse } from '../../domain/protocols'
 
@@ -14,5 +15,14 @@ describe('AxiosAdapter', () => {
     const response = await sut.get('any_url')
     expect(response.status).toBe(200)
     expect(response.data).toEqual({})
+  })
+
+  test('should return an error if no baseURL is provided', async () => {
+    axios.defaults.baseURL = undefined
+    await expect(() => {
+      const sut = new AxiosAdapter()
+      axios.defaults.baseURL = 'https://any.url'
+      return sut
+    }).toThrowError(new Error('Please provided an base URL'))
   })
 })
