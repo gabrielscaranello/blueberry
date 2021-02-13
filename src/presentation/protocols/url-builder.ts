0f1793746fc6198f371f5c43f6ef920ce5349e1f
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-13 09:49:23 -0300

    feat: create url builder

---
+++ src/presentation/protocols/url-builder.ts
@@ -0,0 +1,18 @@
+export interface Query {
+  query?: Array<Record<string, string | number>>
+  params?: Array<string | number>
+  search?: string | number
+  page?: number
+  limit?: number
+}
+
+export interface URLBuilder {
+  uri: string
+  query: Query
+
+  handler: () => string
+  _includeParam: () => URLBuilder
+  _getAditionals: () => Record<string, number | string | unknown>
+  _getQuery: () => Record<string, number | string | unknown>
+  _makeQuery: () => string
+}
---
+++ src/presentation/services/url-builder.ts
@@ -0,0 +1,63 @@
+import { Query, URLBuilder as URLBuilderI } from '../protocols/url-builder'
+
+export class URLBuilder implements URLBuilderI {
+  uri: string
+  query: Query
+
+  constructor (uri: string, query: Query) {
+    this.uri = uri
+    this.query = query
+  }
+
+  handler (): string {
+    this._includeParam()
+    const qs = this._makeQuery()
+    if (!qs) return `${this.uri}`
+    return `${this.uri}?${qs}`
+  }
+
+  _includeParam (): URLBuilder {
+    const { params } = this.query
+    if (!params?.length) return this
+
+    params.forEach(param => {
+      this.uri += `/${param}`
+    })
+
+    return this
+  }
+
+  _getAditionals (): Record<string, number | string | unknown> {
+    const { page, limit, search } = this.query
+    return { page, limit, search }
+  }
+
+  _makeQuery (): string {
+    const params = { ...this._getQuery(), ...this._getAditionals() }
+    const queryString = Object.keys(params)
+      .map(key => {
+        const isValid =
+          Object.prototype.hasOwnProperty.call(params, key) && !!params[key]
+
+        if (!isValid) return ''
+        return `${key}=${encodeURIComponent(params[key] as string)}`
+      })
+      .filter(param => !!param)
+      .join('&')
+
+    return queryString
+  }
+
+  _getQuery (): Record<string, number | string | unknown> {
+    let params = {}
+    if (!this.query.query) return params
+
+    this.query.query.forEach(query => {
+      const [key] = Object.keys(query)
+      const [value] = Object.values(query)
+      params = { ...params, [key]: value }
+    })
+
+    return params
+  }
+}
